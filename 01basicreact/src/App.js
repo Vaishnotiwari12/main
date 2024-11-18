2756dd53fe146c122659b9335315d03db15f7fd2
vaishno tiwari <vaishnotiwari12@gmail.com>
2024-11-18 17:57:15 +0530

    01basicreact

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,15 @@
+
+import chai from "./Chai"
+
+function App() {
+  return (
+   <>
+   <Chai/>
+    
+  
+   </>
+   
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,19 @@
+
+import App from './App.jsx'
+
+
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App | chai </h1>
+    </div>
+  ) 
+}
+
+
+ReactDOM.createRoot(document.getElementById('root'))
+.render(
+ 
+ <reactElement/>
+
+  );
