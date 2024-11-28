8b3ef84b08c8a15f6bff95a1541ae8b81d244e8f
vaishno tiwari <vaishnotiwari12@gmail.com>
2024-11-29 01:09:13 +0530

    reduxToolkitTodo

--- reduxToolkitTodo/src/app/store.js
+++ reduxToolkitTodo/src/app/store.js
@@ -6,7 +6,3 @@ export const store = configureStore({
 })
 
 
-const store = createStore(
-    reducer, /* preloadedState, */
- +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
